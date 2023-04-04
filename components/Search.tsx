8e35059f6005d54c4b0bcdc53d4754b6840af4b0
shimulsust999 <shimulsust999@gmail.com>
2023-04-05 02:16:22 +0600

    redirecting to home page

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,13 @@
 import { useAppDispatch, useAppSelector } from "@/redux/Store";
 import { searched } from "@/redux/filter/FilterSlice";
+import { useRouter } from "next/router";
 import React, { Fragment, useState } from "react";
 
 const Search = () => {
   const { search } = useAppSelector((state) => state.filter);
   const [input, setInput] = useState(search);
   const dispatch = useAppDispatch();
+  const router = useRouter();
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setInput(e.currentTarget.value);
@@ -14,6 +16,7 @@ const Search = () => {
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
     dispatch(searched(input));
+    if (router.asPath !== "/") router.push("/");
   };
 
   return (
